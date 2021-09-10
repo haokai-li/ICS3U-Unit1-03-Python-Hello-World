12526e7bfd3dcd798e9b02e6f5b2e2ac713f65b6
Ubuntu <ubuntu@ip-172-31-68-156.ec2.internal>
2021-09-10 16:43:18 +0000

    hello world

---
+++ hello world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Haokai Li
+# Created on: Sept 2021
+# This is the "Hello world!"Program, with proper style
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
